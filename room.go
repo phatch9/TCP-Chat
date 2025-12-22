b8faad5cf1668858e50ffb73c2162b8858f11fac
Phat <phat.chauu26@gmail.com>
2025-12-21 19:33:32 -0800

    new struct on Room

--- room.go
+++ room.go
@@ -2,11 +2,19 @@ package main
 
 import (
 	"net"
+	"time"
 )
 
+type Message struct {
+	sender    string
+	content   string
+	timestamp time.Time
+}
+
 type room struct {
-	name    string
-	members map[net.Addr]*client
+	name     string
+	members  map[net.Addr]*client
+	messages []Message
 }
 
 func (r *room) broadcast(sender *client, msg string) {
@@ -14,5 +22,17 @@ func (r *room) broadcast(sender *client, msg string) {
 		if sender.conn.RemoteAddr() != addr {
 			m.msg(msg)
 		}
+	}
+}
+
+func (r *room) addMessage(sender *client, content string) {
+	r.messages = append(r.messages, Message{
+		sender:    sender.nick,
+		content:   content,
+		timestamp: time.Now(),
+	})
+	// Keep only last 50 messages
+	if len(r.messages) > 50 {
+		r.messages = r.messages[1:]
 	}
 }
